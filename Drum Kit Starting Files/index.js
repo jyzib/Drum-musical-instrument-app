df72c7a083257c502dd40afd91d820771d9de279
jyzib <jazibzaidi02@gmail.com>
2023-04-23 17:28:36 +0530

    keyboard event listener added

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,57 @@
+let btn = document.querySelectorAll(".drum")
+
+btn.forEach((e)=>{
+   e.addEventListener('click',(m)=>{
+            let button = m.target.innerHTML
+      
+       sundfn(button)
+       animation(button)
+   })
+})
+
+document.addEventListener('keypress', function(e){
+    
+    sundfn(e.key)
+    animation(e.key)
+})
+function sundfn(key){
+    switch(key){
+        case "w":
+            var audio  = new Audio('sounds/crash.mp3') ;
+        audio.play() 
+     break;
+     case "a":
+        var audio  = new Audio('sounds/kick-bass.mp3') ;
+        audio.play() 
+     break;
+     case "s":
+        var audio  = new Audio('sounds/snare.mp3') ;
+        audio.play() 
+     break;
+       case "d":
+        var audio  = new Audio('sounds/tom-1.mp3') ;
+        audio.play() 
+     break;
+     case "j":
+        var audio  = new Audio('sounds/tom-2.mp3') ;
+        audio.play() 
+     break;
+     case "k":
+        var audio  = new Audio('sounds/tom-3.mp3') ;
+        audio.play() 
+     break;
+     case "l":
+        var audio  = new Audio('sounds/tom-4.mp3') ;
+        audio.play() 
+     break;
+     }
+
+ }
+ function animation(click){
+     var neww = document.querySelector('.' + click);
+     neww.classList.add('pressed')
+     setTimeout(function(){
+        neww.classList.remove("pressed")
+     }, 100) 
+
+ } 
